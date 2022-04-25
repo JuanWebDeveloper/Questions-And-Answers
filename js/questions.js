c8d00bef8c18002dea020fbb9f65aec68c920a52
Juan Vargas <juandeveloper19@gmail.com>
2022-04-24 20:50:36 -0500

    Game questions

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,145 @@
+export const firstRoundQuestions = [
+  {
+    question: '¿En que continente esta ubicado Colombia?',
+    options: ['Asia', 'Europa', 'NorteAmerica', 'America'],
+    answer: 'America',
+  },
+  {
+    question: '¿Cuál es el océano más grande del mundo?',
+    options: ['Ocenao Pacifico', 'Ocenao Atlantico', 'Oceano Indico', 'Oceano Aritico'],
+    answer: 'Ocenao Pacifico',
+  },
+  {
+    question: '¿Cual es el resultado de la siguiente operacion 25+25?',
+    options: ['10', '50', '25', '45'],
+    answer: '50',
+  },
+  {
+    question: '¿Que es un algoritmo?',
+    options: [
+      'Un computador super potente',
+      'Figuras geometricas',
+      'Una secuencia de paso para resolver un problema',
+      'Fue el primer satelite en orbita terrestre',
+    ],
+    answer: 'Una secuencia de paso para resolver un problema',
+  },
+  {
+    question: '¿Cual de los siguientes no es cosiderado un lenguaje de programacion?    ',
+    options: ['javascript', 'html', 'java', 'python'],
+    answer: 'html',
+  },
+];
 
+export const secondRoundQuestions = [
+  {
+    question: '¿Con que instrumento se mide la temperatura?    ',
+    options: ['Ventilador', 'Aire acondicionado ', 'Termometro', 'Radiador'],
+    answer: 'Termometro',
+  },
+  {
+    question: '¿Que son los acronimos?',
+    options: ['Todas son correctas', 'Nucleos de complementos acronimales', 'Siglas que se pronuncuan distinto', 'Siglas que se pronuncian como una palabra'],
+    answer: 'Siglas que se pronuncian como una palabra',
+  },
+  {
+    question: '¿Cuál es la capital de España?',
+    options: ['Sevilla', 'Valencia', 'Madrid', 'Valencia'],
+    answer: 'Madrid',
+  },
+  {
+    question: '¿Cual es la capital de Colombia?',
+    options: ['Bogota', 'Medellin', 'Barranquilla', 'Cali'],
+    answer: 'Bogota',
+  },
+  {
+    question: '¿Que palabras esta bien?',
+    options: ['Deduzo', 'pararrallos', 'Jirasol', 'Ninguna de las anteriores'],
+    answer: 'Ninguna de las anteriores',
+  },
+];
+
+export const thirdRoundQuestions = [
+  {
+    question: '¿Cuanto dura en total un partido de futbol?',
+    options: ['85min', '80min', '90min', '90sg'],
+    answer: '90min',
+  },
+  {
+    question: '¿En que año fue el ataque a las torres gemelas?',
+    options: ['1999', '1998', '2000', '2001'],
+    answer: '2001',
+  },
+  {
+    question: '¿QUien pinto la ultima cena?',
+    options: ['Leonardo da Vinci', 'Pablo Picasso', 'Caravaggio', 'Sandro Botticelli'],
+    answer: 'Leonardo da Vinci',
+  },
+  {
+    question: '¿Cuantos astronautas han tenido el privilegio de andar sobre la luna?',
+    options: ['6', '20', '12', '0'],
+    answer: '20',
+  },
+  {
+    question: '¿Que dia se celebra el dia de la tierra?',
+    options: ['23 Mayo', '22 Abril', '31 Diciembre', '24 Diciembre'],
+    answer: '22 Abril',
+  },
+];
+
+export const fourthRoundQuestions = [
+  {
+    question: '¿Cuanto vale el numero pi?',
+    options: ['3,1416', '1,1416', '2,1416', '4,1416'],
+    answer: '3,1416',
+  },
+  {
+    question: '¿Que dia se celebra el Dia de la independencia de Colombia?',
+    options: ['21 Julio', '32 Enero', '20 Junio', '20 Julio'],
+    answer: '20 Julio',
+  },
+  {
+    question: '¿Cual es la persona con mas seguidores en Instagram?',
+    options: ['The Rock', 'Leo Messi', 'Beyonce', 'Cristiano Ronaldo'],
+    answer: 'Cristiano Ronaldo',
+  },
+  {
+    question: 'Tiene pantalla y no es un Tv, Tambien tecla y no es un control remoto, Posee ventilacion y no es aire acondicionado',
+    options: ['Radio', 'Computador', 'Celular', 'Maquina de escribir'],
+    answer: 'Computador',
+  },
+  {
+    question: '¿Que seleccion de futbol ha ganado mas mundiales?',
+    options: ['Colombia', 'Argentina', 'Francia', 'Brasil'],
+    answer: 'Brasil',
+  },
+];
+
+export const fifthRoundQuestions = [
+  {
+    question: 'CUantos vertices tiene un cubo¿?',
+    options: ['12', '8', '10', '6'],
+    answer: '8',
+  },
+  {
+    question: '¿De que color son los guevos de los cocodrilo?',
+    options: ['Blancos', 'Marrones', 'Verdes', 'Amarillos'],
+    answer: 'Blancos',
+  },
+
+  {
+    question: '¿Cual es el continente mas pequeño del planeta tierra?',
+    options: ['Africa', 'America', 'Oceania', 'Europa'],
+    answer: 'Oceania',
+  },
+  {
+    question: '¿Cuantos huesos tiene l cuerpo de una persona adulta?',
+    options: ['307', '208', '190', '206'],
+    answer: '206',
+  },
+  {
+    question: 'Cada componente cromatico de la luz blanca es cuerpos trasnparentes se mueve a velocidad diferente. El color mas rapido en ese sentido es el...',
+    options: ['Amarillo', 'Naranja', 'Rojo', 'Purpura'],
+    answer: 'Rojo',
+  },
+];
