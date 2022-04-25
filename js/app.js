53810d3d416b4b6abaf07169188ed6199e76b82f
Juan Vargas <juandeveloper19@gmail.com>
2022-04-24 20:19:22 -0500

    Hiding start game item and showing quiz box item

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+// Elements HTML:
+const startGame = document.getElementById('startGame');
+const startGameButton = document.getElementById('startGameButton');
+const quizBox = document.getElementById('quizBox');
+
+// Events Listeners
+startGameButton.addEventListener('click', () => {
+  startGame.classList.remove('show');
+  quizBox.classList.add('show');
+});
